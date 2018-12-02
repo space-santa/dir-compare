82eee85f69fa7a274e4847424c4b882219f638dd
claus <claus.z@protonmail.com>
2018-12-02 18:06:48 +1100

    Implement FileOutput

--- DirCompare/FileOutput.cs
+++ DirCompare/FileOutput.cs
@@ -1,12 +1,26 @@
 using System.Collections.Generic;
+using System.IO;
 
 namespace DirCompare
 {
     public class FileOutput : IOutput
     {
+        private readonly string _path;
+
+        public FileOutput(string path)
+        {
+            _path = path;
+        }
+
         public void Write(List<string> md5sums)
         {
-            //
+            using (StreamWriter sw = new StreamWriter(_path, false))
+            {
+                foreach (var line in md5sums)
+                {
+                    sw.WriteLine(line);
+                }
+            }
         }
     }
 }
