02cad74f43b164591fc901c00a14776d0ce5c82b
claus <claus.z@protonmail.com>
2018-12-02 15:06:29 +1100

    Add stub for FileOutput

---
+++ DirCompare/FileOutput.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace DirCompare
+{
+    public class FileOutput : IOutput
+    {
+        public void Write(List<string> md5sums)
+        {
+            //
+        }
+    }
+}
