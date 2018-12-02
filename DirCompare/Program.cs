92414b2b4ef8a51946022f0ae9a3350cf1ceed08
claus <claus.z@protonmail.com>
2018-12-02 18:06:19 +1100

    Add proper command line arg parsing

--- DirCompare/Program.cs
+++ DirCompare/Program.cs
@@ -1,11 +1,36 @@
-﻿namespace DirCompare
+﻿using System;
+using CommandLine;
+
+namespace DirCompare
 {
     class Program
     {
+        class Options
+        {
+            [Option('o', "output", Default="", HelpText = "File to write the result to. If this is not set, the result will be written to stdout.")]
+            public string OutputFile { get; set; }
+
+            [Value(0, Required = true, MetaName = "basedir", HelpText = "The directory that is scanned recursively.")]
+            public string basedir { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            var md5sums = new RecursiveMD5ListOfDirectory(args[0], new ConsoleOutput());
-            md5sums.Write();
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    IOutput output;
+                    if (o.OutputFile.Length > 0)
+                    {
+                        output = new FileOutput(o.OutputFile);
+                    }
+                    else
+                    {
+                        output = new ConsoleOutput();
+                    }
+                    var md5sums = new RecursiveMD5ListOfDirectory(o.basedir, output);
+                    md5sums.Write();
+                });
         }
     }
 }
