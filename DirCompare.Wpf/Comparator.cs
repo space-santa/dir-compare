372a0ccd09bf259f1ab08a57dc9a6de3f5e143bc
space-santa <claus.z@protonmail.com>
2019-04-16 18:50:41 +1000

    Add directory paths to compare output

--- DirCompare.Wpf/Comparator.cs
+++ DirCompare.Wpf/Comparator.cs
@@ -5,18 +5,33 @@ namespace DirCompare.Wpf
 {
     public class Comparator
     {
+        private static List<string> ListHeader(string folder1, string folder2)
+        {
+            List<string> listHeader = new List<string>();
+            listHeader.Add($"a = {folder1}");
+            listHeader.Add($"b = {folder2}");
+            listHeader.Add("-------------------");
+            return listHeader;
+        }
         public static List<string> Compare(string folder1, string folder2)
         {
             var md5sums = new RecursiveMD5ListOfDirectory(folder1);
+            List<string> diffList;
 
             if (folder2.Length > 0)
             {
                 var secondSums = new RecursiveMD5ListOfDirectory(folder2);
                 var diffSums = md5sums.Diff(secondSums);
-                return diffSums.GetPathsWithMD5Sum();
+                diffList = diffSums.GetPathsWithMD5Sum();
+            }
+            else
+            {
+                diffList = md5sums.GetPathsWithMD5Sum();
             }
 
-            return md5sums.GetPathsWithMD5Sum();
+            var comparedList = ListHeader(folder1, folder2);
+            comparedList.AddRange(diffList);
+            return comparedList;
         }
     }
 }
