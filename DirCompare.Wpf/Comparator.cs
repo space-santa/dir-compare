a572db34c40fd2bee1d137e6cd07014c597d1f42
space-santa <claus.z@protonmail.com>
2019-04-16 18:37:30 +1000

    Remove the IOutput injection nonsense
    What was I thinking when I wrote that?

--- DirCompare.Wpf/Comparator.cs
+++ DirCompare.Wpf/Comparator.cs
@@ -3,35 +3,20 @@ using System.Collections.Generic;
 
 namespace DirCompare.Wpf
 {
-    class ListOutput : DirCompare.IOutput
-    {
-        public List<string> result { get; set; }
-
-        public void Write(List<string> md5sums)
-        {
-            result = md5sums;
-        }
-    }
-
     public class Comparator
     {
         public static List<string> Compare(string folder1, string folder2)
         {
-            var output = new ListOutput();
+            var md5sums = new RecursiveMD5ListOfDirectory(folder1);
 
-            var md5sums = new RecursiveMD5ListOfDirectory(folder1, output);
             if (folder2.Length > 0)
             {
-                var secondSums = new RecursiveMD5ListOfDirectory(folder2, output);
+                var secondSums = new RecursiveMD5ListOfDirectory(folder2);
                 var diffSums = md5sums.Diff(secondSums);
-                diffSums.Write();
-            }
-            else
-            {
-                md5sums.Write();
+                return diffSums.GetPathsWithMD5Sum();
             }
 
-            return output.result;
+            return md5sums.GetPathsWithMD5Sum();
         }
     }
 }
