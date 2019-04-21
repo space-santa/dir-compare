5c8038039062fae680482762a240fb79a9142685
space-santa <claus.z@protonmail.com>
2019-04-21 10:27:56 +1000

    Add one-click msi build

---
+++ wix/Publish.ps1
@@ -0,0 +1,16 @@
+$version = "1.0.1"
+$PathToPublish = "..\DirCompare.Wpf\bin\Release\netcoreapp3.0\publish"
+
+dotnet publish ../DirCompare.Wpf/DirCompare.Wpf.csproj -c Release
+
+[XML]$xml = Get-Content "$pwd\\main.wxs"
+$xml.Wix.Product.SetAttribute("Version", $version)
+$xml.Save("$pwd\\main.wxs")
+
+heat.exe dir $PathToPublish -cg DependencyGroup -dr INSTALLDIR -gg -g1 -out DependencyGroup.wxs
+(Get-Content .\DependencyGroup.wxs) | ForEach-Object {
+    $_ -replace "SourceDir", $PathToPublish
+} | Set-Content .\DependencyGroup.wxs
+
+candle.exe main.wxs DependencyGroup.wxs
+light.exe  -out "$pwd\\msi\\DirCompare-${version}.msi" main.wixobj DependencyGroup.wixobj
--- wix/create-msi.ps1
+++
@@ -1,2 +0,0 @@
-candle.exe main.wxs DependencyGroup.wxs
-light.exe  -out DirCompare-1.0.0.msi main.wixobj DependencyGroup.wixobj
